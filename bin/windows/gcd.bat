054f014253146dea15a2a98673e0af137aa2f873
sago35 <sago35@gmail.com>
2020-05-29 10:37:57 +0900

    Update gcd.bat

--- bin/windows/gcd.bat
+++ bin/windows/gcd.bat
@@ -1,8 +1,10 @@
 @echo off
 
-for /f %%i in ('ghq list -p ^| peco') do (
+for /f %%i in ('ghq list --vcs git -p ^| peco') do (
     if "%~1" == "-e" (
         start %%i
+    ) else if "%~1" == "-c" (
+        start cmd /k cd %%i
     ) else if "%~1" == "-v" (
         cmd /c start gvim %%i
     ) else (
