215daaa47f141c1900faae49ea8d4ce61f9c00fc
sago35 <sago35@gmail.com>
2020-09-30 11:48:49 +0900

    Update setup.pl

--- setup.pl
+++ setup.pl
@@ -9,16 +9,40 @@ if (0 == scalar @ARGV) {
     exit 1;
 }
 
-my $setting = {
+my $ln_settings = {
     'tmux/conf'  => "$ENV{HOME}/.tmux.conf",
     'vim/vimrc'  => "$ENV{HOME}/.vimrc",
     'git/config' => "$ENV{HOME}/.gitconfig",
+    'tig/tigrc'  => "$ENV{HOME}/.tigrc",
 };
 
+my @mkdirs = (
+    "$ENV{HOME}/.vim",
+    "$ENV{HOME}/.vim/backup/",
+    "$ENV{HOME}/.vim/swap/",
+    "$ENV{HOME}/.vim/undo/",
+);
+
 if ($ARGV[0] eq "install") {
-    foreach my $key (keys %{$setting}) {
-        ln($key, $setting->{$key});
+    foreach my $key (keys %{$ln_settings}) {
+        if (-e $ln_settings->{$key}) {
+            printf "skip ln %s\n", $ln_settings->{$key};
+        } else {
+            ln($key, $ln_settings->{$key});
+        } 
     }
+
+    foreach my $dir (@mkdirs) {
+        if (-e $dir) {
+            printf "skip mkdir %s\n", $dir;
+        } else {
+            printf "mkdir %s\n", $dir;
+            mkdir $dir or die $!;
+        } 
+    }
+
+
+
 }
 
 sub ln {
